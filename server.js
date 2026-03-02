2245ae295c94235aa414960d7d5892e48dec3d37
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-03-02 23:52:19 +0600

    added all resources server file

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 3500;
+
+// allowed multiple origins
+const allowedOrigins = ["http://localhost:5173"];
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+
+
+app.get("/", (req, res) => {
+    res.send("G-Mart server is running...");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
