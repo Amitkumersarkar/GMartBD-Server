bbb6f97ed6e8bee21506df4a757acffac4debf7a
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-03-05 00:23:57 +0600

    created user apis

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,3 +4,5 @@ import { register } from '../controller/userController.js';
 const userRouter = express.Router();
 
 userRouter.post('/register', register)
+
+export default userRouter;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import 'dotenv/config';
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 const port = process.env.PORT || 3500;
@@ -17,10 +18,10 @@ app.use(cookieParser());
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 // routes
-
 app.get("/", (req, res) => {
     res.send("G-Mart server is running...!!");
 });
+app.use('/api/user', userRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
