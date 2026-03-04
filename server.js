d7d17341b9846ea31110cf6d2e2d25d563445056
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-03-04 21:39:04 +0600

    updated dotenv config file

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import connectDB from "./config/db.js";
+import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 3500;
 
+await connectDB()
 // allowed multiple origins
 const allowedOrigins = ["http://localhost:5173"];
 
