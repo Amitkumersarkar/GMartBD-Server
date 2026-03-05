780563fb68611c0a0b62e8221aefb84bbb41956d
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-03-05 20:42:39 +0600

    here is the Final API Structure

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import { isAuth, login, logout, register } from '../controller/userController.js';
-import authUser from '../middlewares/authUser.js';
+import express from "express";
+import { isAuth, login, logout, register } from "../controller/userController.js";
+import authUser from "../middlewares/authUser.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/register', register)
-userRouter.post('/login', login)
-userRouter.get('/is-auth', authUser, isAuth)
-userRouter.get('/logout', authUser, logout)
+userRouter.post("/register", register);
+userRouter.post("/login", login);
+userRouter.get("/is-auth", authUser, isAuth);
+userRouter.post("/logout", authUser, logout);
 
 export default userRouter;
