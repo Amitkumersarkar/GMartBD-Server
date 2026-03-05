812838a702ba1b41c448023cd7c93bfe8939e11b
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-03-05 22:36:23 +0600

    updated version of authSeller

--- middlewares/authSeller.js
+++ middlewares/authSeller.js
@@ -1,5 +1,36 @@
 import jwt from "jsonwebtoken";
 
-const authSeller = async (req, res, next) => {
+const authSeller = (req, res, next) => {
+    const { sellerToken } = req.cookies;
 
-}
+    if (!sellerToken) {
+        return res.status(401).json({
+            success: false,
+            message: "Not Authorized"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(sellerToken, process.env.JWT_SECRET);
+
+        if (!decoded.email) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid Token"
+            });
+        }
+
+        // Attach seller info to request
+        req.sellerEmail = decoded.email;
+
+        next();
+
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "Token verification failed"
+        });
+    }
+};
+
+export default authSeller;
