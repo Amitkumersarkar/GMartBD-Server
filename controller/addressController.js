5c79b3d05f378d2f13e619d07846f5421df5ff2a
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-03-07 20:30:24 +0600

    tryCatch block error fix

--- controller/addressController.js
+++ controller/addressController.js
@@ -1,7 +1,25 @@
+import Address from "../models/address";
 
 
 // Add Address : /api/address/add
+export const addAddress = async (req, res) => {
+    try {
+        const { address, userId } = req.body;
+        await Address.create({ ...address, userId })
+        res.json({ success: true, message: "Address added successfully" })
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
 
-export const addAddress = async () => {
+    }
+}
+
+// Get Address: /api/address/get
+export const getAddress = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
 
 }
