b68f9cd4ea262528dc27a896dcdda98bf2d6a116
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-03-07 21:38:18 +0600

    order models done

--- models/order.js
+++ models/order.js
@@ -1,8 +1,18 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
+    userId: { type: String, requires: true, ref: 'user' },
+    items: [{
+        product: { type: String, requires: true, ref: 'product' },
+        quantity: { type: Number, requires: true, }
+    }],
+    amount: { type: Number, requires: true, },
+    address: { type: String, requires: true, ref: 'address' },
+    status: { type: String, default: 'Order Placed' },
+    paymentType: { type: String, required: true },
+    isPaid: { type: Boolean, required: true, default: false },
 
-})
+}, { timestamps: true })
 
 const Order = mongoose.models.order || mongoose.model('order', orderSchema)
 
