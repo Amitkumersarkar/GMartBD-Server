47cca88bc0fabdc54953cb5f0d4940c32a282994
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-03-08 00:59:54 +0600

    completed all apis on the backend

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import connectCloudinary from "./config/cloudinary.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import addressRouter from "./routes/addressRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
 const port = process.env.PORT || 3500;
@@ -34,6 +35,7 @@ app.use("/api/seller", sellerRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/address", addressRouter);
+app.use("/api/order", orderRouter);
 
 
 // start server after DB connects
