ad46eda3b898ad930ae0e0cd6211fcc50447919e
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-03-07 13:15:28 +0600

    cloudinary set up done

---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from "cloudinary"
+
+const connectCloudinary = async () => {
+    cloudinary.config({
+        cloud_name:
+            api_key:
+        api_secret:
+    })
+}
